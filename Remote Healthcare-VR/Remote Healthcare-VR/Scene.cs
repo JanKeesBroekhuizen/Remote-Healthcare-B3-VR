2ebb220626cbca7832c491021d6a2be8df7583c0
Kasper <kh.adan@student.avans.nl>
2020-09-16 16:10:24 +0200

    [Feature] scene.node help methods

--- Remote Healthcare-VR/Remote Healthcare-VR/Scene.cs
+++ Remote Healthcare-VR/Remote Healthcare-VR/Scene.cs
@@ -41,12 +41,186 @@ namespace Remote_Healthcare_VR
             return save.ToString();
         }
 
+        public static string Load(string fileName)
+        {
+            JObject load = 
+                new JObject(
+                    new JProperty("id", "scene/load"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("filename", fileName))));
+            return load.ToString();
+        }
 
+        public static string Raycast(int[] start, int[] direction, bool physics)
+        {
+            JObject raycast =
+                new JObject(
+                    new JProperty("id", "scene/raycast"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("start", start), 
+                        new JProperty("direction", direction),
+                        new JProperty("physics", physics))));
+            return raycast.ToString();
+        }
     }
     class Node
     {
-        
+        public static string Add(string name, string guid, 
+            int[] position, float scale, int[] rotation,
+            string filename, bool cullbackfaces, bool animated, string animationname,
+            bool smoothnormals, int[] panelSize, int[] panelResolution, int[] background, bool castshadow,
+            int[] waterSize, float waterResolution)
+        {
+            JObject add =
+                new JObject(
+                    new JProperty("id", "scene/node/add"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("name", name),
+                        new JProperty("parent", guid),
+                        new JProperty("components",
+                        new JObject(
+                            new JProperty("transform", 
+                            new JObject(
+                                new JProperty("position", position),
+                                new JProperty("scale", scale),
+                                new JProperty("rotation", rotation))),
+                            new JProperty("model",
+                            new JObject(
+                                new JProperty("file", filename),
+                                new JProperty("cullbackfaces", cullbackfaces),
+                                new JProperty("animated", animated),
+                                new JProperty("animation", animationname))),
+                            new JProperty("terrain",
+                            new JObject(
+                                new JProperty("smoothnormals", smoothnormals))),
+                            new JProperty("panel",
+                            new JObject(
+                                new JProperty("size", panelSize),
+                                new JProperty("resolution", panelResolution),
+                                new JProperty("background", background),
+                                new JProperty("castShadow", castshadow))),
+                            new JProperty("water",
+                            new JObject(
+                                new JProperty("size", waterSize),
+                                new JProperty("resolulion", waterResolution))))))));
+            return add.ToString();
+        }
 
+        public static string Update(string id, string parent, int[] position, float scale, int[] rotation, string name, float speed)
+        {
+            JObject update =
+                new JObject(
+                    new JProperty("id", "scene/node/update"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("id", id),
+                        new JProperty("parent", parent),
+                        new JProperty("transform",
+                        new JObject(
+                            new JProperty("posotion", position),
+                            new JProperty("scale", scale),
+                            new JProperty("rotation", rotation)),
+                        new JProperty("animation", 
+                        new JObject(
+                            new JProperty("name", name),
+                            new JProperty("speed", speed)))))));
+            return update.ToString();
+        }
+
+        public static string MoveTo(string id, int[] position, string rotate, string interpolate, bool followheight, int time)
+        {
+            JObject moveto =
+                new JObject(
+                    new JProperty("id", "scene/node/moveto"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("id", id),
+                        new JProperty("position", position),
+                        new JProperty("rotate", rotate),
+                        new JProperty("interpolate", interpolate),
+                        new JProperty("followheight", followheight),
+                        new JProperty("time", time))));
+            return moveto.ToString();
+        }
+
+        public static string MoveTo(string id, int[] position, string rotate, string interpolate, bool followheight, float speed)
+        {
+            JObject moveto =
+                new JObject(
+                    new JProperty("id", "scene/node/moveto"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("id", id),
+                        new JProperty("position", position),
+                        new JProperty("rotate", rotate),
+                        new JProperty("interpolate", interpolate),
+                        new JProperty("followheight", followheight),
+                        new JProperty("speed", speed))));
+            return moveto.ToString();
+        }
+
+        public static string MoveTo(string id, string stop)
+        {
+            JObject moveto =
+                new JObject(
+                    new JProperty("id", "scene/node/moveto"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("id", id),
+                        new JProperty("stop", stop))));
+            return moveto.ToString();
+        }
+
+        public static string Delete(string id)
+        {
+            JObject delete =
+                new JObject(
+                    new JProperty("id", "scene/node/delete"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("id", id))));
+            return delete.ToString();
+        }
+
+        public static string Find(string name)
+        {
+            JObject find =
+                new JObject(
+                    new JProperty("id", "scene/node/find"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("name", name))));
+            return find.ToString();
+        }
+
+        public static string AddLayer(string id, string diffuse, string normal, int minHeight, int maxHeight, float fadeDist)
+        {
+            JObject addLayer =
+                new JObject(
+                    new JProperty("id", "scene/node/addlayer"),
+                    new JProperty("data",
+                    new JObject(
+                        new JProperty("id", id),
+                        new JProperty("diffuse", diffuse),
+                        new JProperty("normal", normal),
+                        new JProperty("minHeight", minHeight),
+                        new JProperty("maxHeight", maxHeight),
+                        new JProperty("fadeDist", fadeDist))));
+            return addLayer.ToString();
+        }
+
+        public static string DelLayer(string name)
+        {
+            JObject delLayer =
+                new JObject(
+                    new JProperty("id", "scene/node/find"),
+                    new JProperty("data",
+                    new JObject()));
+            return delLayer.ToString();
+        }
     } 
 
     class Terrain
