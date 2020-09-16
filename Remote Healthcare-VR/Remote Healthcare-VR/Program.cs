34239a399794dd0574f882e92d77f909a7fe9d2c
Jan Kees <jc.broekhuizen@student.avans.nl>
2020-09-16 10:02:18 +0200

    [Add] simple client

--- Remote Healthcare-VR/Remote Healthcare-VR/Program.cs
+++ Remote Healthcare-VR/Remote Healthcare-VR/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
 
-namespace Remote_Healthcare_VR
+namespace SimpleTCPClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TcpClient client = new TcpClient("145.48.6.10", 6666);
+            bool done = false;
+            Console.WriteLine("Type 'bye' to end connection");
+            while (!done)
+            {
+                Console.Write("Enter a message to send to server: ");
+                string message = Console.ReadLine();
+
+                WriteTextMessage(client, message);
+
+                string response = ReadTextMessage(client);
+                Console.WriteLine("Response: " + response);
+                done = response.Equals("BYE");
+            }
+        }
+
+        public static void WriteTextMessage(TcpClient client, string message)
+        {
+            var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            {
+                stream.WriteLine(message);
+                stream.Flush();
+            }
+        }
+
+        public static string ReadTextMessage(TcpClient client)
+        {
+            var stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+            {
+                return stream.ReadLine();
+            }
         }
     }
 }
