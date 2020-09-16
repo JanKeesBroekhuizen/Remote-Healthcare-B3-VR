ca1eb46127da761791a7563c539698b9c11b06a1
Kasper <kh.adan@student.avans.nl>
2020-09-16 14:12:19 +0200

    [Feature] Skeleton for help functions

---
+++ Remote Healthcare-VR/Remote Healthcare-VR/General.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remote_Healthcare_VR
+{
+    class General
+    {
+    }
+}
---
+++ Remote Healthcare-VR/Remote Healthcare-VR/Scene.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remote_Healthcare_VR
+{
+    class Scene
+    {
+        public static Node Node = new Node();
+        public static Terrain Terrain = new Terrain();
+        public static Panel Panel = new Panel();
+        public static Skybox Skybox = new Skybox();
+        public static Road Road = new Road();
+
+
+
+
+    }
+    class Node
+    {
+
+    } 
+
+    class Terrain
+    {
+
+    }
+
+    class Panel
+    {
+
+    }
+
+    class Skybox
+    {
+
+    }
+
+    class Road
+    {
+
+    }
+}
+
