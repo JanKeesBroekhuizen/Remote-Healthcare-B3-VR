3bb88ec654802abb7fb0de0d2046ad69a3e290db
Kasper <kh.adan@student.avans.nl>
2020-09-16 09:49:33 +0200

    [Initial commit]

---
+++ Remote Healthcare-VR/Remote Healthcare-VR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Remote_Healthcare_VR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
