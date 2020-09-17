60a9378d7a3c7f2ce36628b143fb36fe99dadb88
Jorn Neijssen <jorn.neijssen@hotmail.com>
2020-09-17 14:06:42 +0200

    [Add]  Terrain and skybox methods (skybox update method not ready yet.

--- Remote Healthcare-VR/Remote Healthcare-VR/Scene.cs
+++ Remote Healthcare-VR/Remote Healthcare-VR/Scene.cs
@@ -53,25 +53,37 @@ namespace Remote_Healthcare_VR
     {
         public static string Add(float[] size, float[] heights)
         {
-            JObject add = new JObject(new JProperty("id", "scene/terrain/add"), new JProperty("data", new JObject(new JProperty("size", size), new JProperty("heights", heights))));
+            JObject add = new JObject(
+                new JProperty("id", "scene/terrain/add"),
+                new JProperty("data", new JObject(new JProperty("size", size),
+                new JProperty("heights", heights))));
             return add.ToString();
         }
 
         public static string Update()
         {
-            JObject update = new JObject(new JProperty("id", "scene/terrain/update"), new JProperty("data"));
+            JObject update = new JObject(
+                new JProperty("id", "scene/terrain/update"),
+                new JProperty("data"));
             return update.ToString();
         }
 
         public static string Delete()
         {
-            JObject delete = new JObject(new JProperty("id", "scene/terrain/delete"), new JProperty("data"));
+            JObject delete = new JObject(
+                new JProperty("id", "scene/terrain/delete"),
+                new JProperty("data"));
             return delete.ToString();
         }
 
         public static string GetHeight(float[] position, float[] positions)
         {
-            JObject getheight = new JObject(new JProperty("id", "scene/terrain/getheight"), new JProperty("data", new JObject(new JProperty("position", position), new JProperty("positions", positions))));
+            JObject getheight = new JObject(
+                new JProperty("id", "scene/terrain/getheight"),
+                new JProperty("data",
+                new JObject(
+                    new JProperty("position", position),
+                    new JProperty("positions", positions))));
             return getheight.ToString();
         }
 
@@ -86,8 +98,30 @@ namespace Remote_Healthcare_VR
     {
         public static string SetTime(string time)
         {
-            JObject settime = new JObject(new JProperty("id", "scene/skybox/settime"), new JProperty("data", new JObject(new JProperty("time", time))));
+            JObject settime = new JObject(
+                new JProperty("id", "scene/skybox/settime"), 
+                new JProperty("data", 
+                new JObject(
+                    new JProperty("time", time))));
             return settime.ToString();
+        }
+
+        public static string Update()
+        {
+            JObject update = new JObject(
+                new JProperty("id", "scene/terrain/update"),
+                new JProperty("data", 
+                new JObject(
+                    new JProperty("type", "static"),
+                    new JProperty("files", 
+                    new JObject(
+                        new JProperty("xpos", "data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_rt.png"),
+                        new JProperty("xneg", "data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_lf.png"),
+                        new JProperty("ypos", "data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_up.png"),
+                        new JProperty("yneg", "data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_dn.png"),
+                        new JProperty("zpos", "data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_bk.png"),
+                        new JProperty("zneg", "data/NetworkEngine/textures/SkyBoxes/interstellar/interstellar_ft.png"))))));
+            return update.ToString();
         }
 
     }
